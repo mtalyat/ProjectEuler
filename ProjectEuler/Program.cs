0098838f56c6b4c0cbf25b40eee33d895a892ebf
Mitchell Talyat <mitchell.talyat@gmail.com>
2022-04-30 17:38:54 -0500

    Problem 12

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -6,7 +6,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Problem problem = new Problem11();
+        Problem problem = new Problem12();
 
         problem.DoWork();
     }
