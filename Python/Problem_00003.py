c7b5a7d79dd3883dad27888f9cadb6d48adff1c2
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2026-04-12 08:42:45 -0500

    Update Problem_00003.py
    Add thoughts

--- Python/Problem_00003.py
+++ Python/Problem_00003.py
@@ -3,6 +3,9 @@ ProjectEuler.net #3
 
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
+
+The thought here is that we can keep breaking down the number by removing the factors.
+Eventually we are left with the largest factor.
 """
 
 # NUMBER = 13195
